43d08e8eb5f3e1f62ce4914d1bc74faff3ddf4ea
handakim25 <handakim25@gmail.com>
2025-02-04 12:52:14 +0900

    Initial Commit: Unreal Project 5.5.2 with Starter Content
    - unreal 5.5.2
    - C++
    - Scalable

---
+++ Source/Sparta_Practice_7/Sparta_Practice_7.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Sparta_Practice_7.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Sparta_Practice_7, "Sparta_Practice_7" );
